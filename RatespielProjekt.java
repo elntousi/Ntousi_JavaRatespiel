d34c8ebfdc1ee155dc3a240fa3052ee4994ffed7
elntousi <e.ntousi@edu.bbq.de>
2025-06-10 14:27:52 +0200

    Update2 RatespielProjekt.java

--- RatespielProjekt.java
+++ RatespielProjekt.java
@@ -11,7 +11,7 @@ public class RatespielProjekt {
         boolean nochmalSpielen = true;
 
         while (nochmalSpielen) {
-            int geheimZahl = random.nextInt(11);
+            int geheimZahl = random.nextInt(11); 
 
             System.out.println("Ich habe eine Zahl zwischen 0 und 10 ausgedacht. Rate sie!");
 
@@ -19,23 +19,30 @@ public class RatespielProjekt {
 
             while (true) {
                 System.out.print("Gebe die Zahl ein und drücke die Enter-Taste: ");
-                eingabe = scanner.nextInt();
 
-                if (eingabe == geheimZahl) {
-                    System.out.println("Richtig!");
-                    break;
-                } else {
-                    System.out.println("Falsch! Versuch es noch mal!");
+                if (scanner.hasNextInt()) {
+                    eingabe = scanner.nextInt();
+
+                    if (eingabe == geheimZahl) {
+                        System.out.println("Richtig!");
+                        break;
+                    } else {
+                        System.out.println("Falsch! Versuch es noch mal!");
+                    }
+                    } else {
+                        System.out.println("Bitte gib eine Zahl zwischen 0 und 10 ein.");
+                    scanner.next(); //
                 }
             }
 
             System.out.print("Noch mal spielen? Wenn ja, tippe 1 (und Enter), sonst egal was (und Enter): ");
-            int nochmal = scanner.nextInt();
 
-            if (nochmal == 1) {
-                nochmalSpielen = true;
+            if (scanner.hasNextInt()) {
+                int nochmal = scanner.nextInt();
+                nochmalSpielen = (nochmal == 1);
             } else {
                 nochmalSpielen = false;
+                scanner.next();
                 System.out.println("Tschüss! Bis zum nächsten Mal :-)");
             }
         }
@@ -43,4 +50,3 @@ public class RatespielProjekt {
         scanner.close();
     }
 }
- 
